92832f8ec923b05051c817e54e6ce9c7db5883d1
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-07-27 02:39:32 +0100

    ci: loading state for table

--- src/features/Leaderboard/Leaderboard.Module.tsx
+++ src/features/Leaderboard/Leaderboard.Module.tsx
@@ -2,13 +2,22 @@ import Section from '@components/elements/Section';
 import { useFetchLeaderBoard } from './api';
 import TableComponent from '@components/elements/Table';
 import Header from './components/Header';
+import BtnLoader from '@components/elements/Button/loader';
 
 const HomeModule = () => {
   const { leaderBoard, leadColumns, loadingLeaderBoard } =
     useFetchLeaderBoard();
+
   return (
     <Section>
       <Header />
+      {loadingLeaderBoard && (
+        <Section className="h-64 w-full flex items-center justify-center">
+          <Section>
+            <BtnLoader />
+          </Section>
+        </Section>
+      )}
       {leaderBoard?.directory_items.length > 0 && (
         <TableComponent
           data={leaderBoard?.directory_items}
